397ddec75dbc589e4cd6a26d2015c46de660219f
winny1212 <maomaodragon1212@gmail.com>
2021-12-06 21:30:12 +1100

    add the login form

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input type="text" id="email" name="email" />
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" name="password" />
+
+      <button role="button">submit</button>
+    </form>
+  );
+}
