428ae396b5e2cb1bf58af46369f8c52cbdaef7f7
winny1212 <maomaodragon1212@gmail.com>
2021-12-06 21:32:05 +1100

    import fireEvent and input validation for the testing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,3 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('App renders normally', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Hello!! DIY GROOMING Team!');
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App, { validateInput } from './App';
