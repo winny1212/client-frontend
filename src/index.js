7004ff5d2968741c9ccd092f05dd198596e22b06
thangzathang <thangzathang@gmail.com>
2021-12-07 12:15:01 +1100

    Fixed app location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './components/App';
+import App from './App';
 
 // Redux - our state manager.
 import { Provider } from 'react-redux';
