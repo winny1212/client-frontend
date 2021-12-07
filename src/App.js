3f15d9af59056e3c160461dc1f909a5f552a691e
thangzathang <thangzathang@gmail.com>
2021-12-07 16:17:16 +1100

    tested dispatch works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
 import Login from './components/Users/Login/Login';
+import { useEffect } from 'react';
+
+// Redux Connection
+import { useDispatch, useSelector } from 'react-redux';
+
+// Get our actions from actions folder.
+import { getAllPosts } from './actions/posts';
 
 function App() {
+  /*
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+     dispatch(getAllPosts());
+   }, [dispatch]);
+
+   const posts = useSelector((state) => state.postsReducer);
+
+   console.log(posts);
+
+  */
+
   return (
     <div className="App">
       <h1>Hello!! DIY GROOMING Team!</h1>
