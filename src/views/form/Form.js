ece55e65c44bbfb89fe1b4caf15890bbeb7d9861
thangzathang <thangzathang@gmail.com>
2021-12-08 12:15:22 +1100

    Barely basic a form

--- src/views/form/Form.js
+++ src/views/form/Form.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+// Custom style hook
+import useStyles from './styles';
+
+// Material UI
+import { Paper, Container, TextField, Typography } from '@mui/material';
+
 function Form() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Create a Form here!</h1>
-    </div>
+    <Container fixed>
+      <Paper elevation={2}>
+        <Typography variant="h3" gutterBottom component="div" pt={2} mt={2}>
+          Create A Post
+        </Typography>
+      </Paper>
+    </Container>
   );
 }
 
