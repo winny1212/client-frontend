7dad75e2ba42e2b0d78f5857d880bdb25364bb78
thangzathang <thangzathang@gmail.com>
2021-12-08 16:29:26 +1100

    Form page is created

--- src/views/form/Form.js
+++ src/views/form/Form.js
@@ -1,20 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Custom style hook
 import useStyles from './styles';
 
 // Material UI
-import { Paper, Container, TextField, Typography } from '@mui/material';
+import { Paper, Container, TextField, Typography, Button } from '@mui/material';
 
 function Form() {
+  // Invoke the styles
   const classes = useStyles();
+
+  // Post state
+  const [postData, setPostData] = useState({
+    title: '',
+    videoLink: '',
+    instructions: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+    console.log(postData);
+  };
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setPostData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
   return (
     <Container fixed>
-      <Paper elevation={2}>
-        <Typography variant="h3" gutterBottom component="div" pt={2} mt={2}>
-          Create A Post
-        </Typography>
+      <Paper elevation={2} className={classes.paper}>
+        <form
+          className={`${classes.form} ${classes.root}`}
+          autoComplete="off"
+          noValidate
+          onSubmit={handleSubmit}
+        >
+          <Typography variant="h4" gutterBottom component="div" pt={2} mt={2}>
+            Create A Post
+          </Typography>
+          <TextField
+            autoFocus={true}
+            fullWidth
+            required
+            variant="outlined"
+            name="title"
+            autoComplete="off"
+            label="Title"
+            color="secondary"
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            fullWidth
+            required
+            variant="outlined"
+            name="videoLink"
+            autoComplete="off"
+            label="Video Link"
+            color="secondary"
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="instructions"
+            variant="outlined"
+            label="Instructions"
+            color="secondary"
+            fullWidth
+            multiline
+            rows={4}
+            required
+            onChange={(e) => handleChange(e)}
+          />
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            size="large"
+            fullWidth
+          >
+            Submit
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
--- src/views/form/styles.js
+++ src/views/form/styles.js
@@ -1,5 +1,19 @@
 import { makeStyles } from '@material-ui/core';
 
 export default makeStyles((style) => ({
-  mainContainer: {},
+  form: {
+    maxWidth: '800px',
+    margin: '2rem auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  root: {
+    '& .MuiTextField-root': {
+      margin: style.spacing(1),
+    },
+  },
+  paper: {
+    padding: style.spacing(2),
+  },
 }));
