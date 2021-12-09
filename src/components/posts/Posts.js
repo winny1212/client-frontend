c606ecd6724ae74da0c95bf3f014db48363b57c0
a-sh <iam.angie@ymail.com>
2021-12-09 18:19:30 +1100

    Add simple footer

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Post from './post/Post';
-import data from '../../data/postsData';
-// import { fetchAllPosts } from '../../api';
 // import { CircularProgress } from '@mui/material';
 // import Grid from '@mui/material/Grid';
 // import Paper from '@mui/material/Paper';
@@ -10,11 +8,11 @@ import data from '../../data/postsData';
 function Posts() {
   const posts = useSelector((state) => state.postsReducer);
   console.log(`-- checking --\n${posts}`);
+
   return (
     <div>
-      <h3>List of Posts</h3>
+      <h3>List of Posts (in cards)</h3>
       {/* Will map the posts later once the card is ready */}
-      <small>list of posts in cards</small>
 
       {/* <Grid container>
         <Grid item>
@@ -37,12 +35,6 @@ function Posts() {
           </div>
         );
       })}
-
-      {/* <hr />
-      <small>test with local fake data</small>
-      {data.map((post) => {
-        return <p key={post._id}>{post.title}</p>;
-      })} */}
     </div>
   );
 }
