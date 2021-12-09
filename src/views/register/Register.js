b95af35f882c52c19479646ea6673e784fee70d2
winny1212 <maomaodragon1212@gmail.com>
2021-12-10 10:41:27 +1100

    set up the register form container and import the lock avatar

--- src/views/register/Register.js
+++ src/views/register/Register.js
@@ -7,7 +7,16 @@ import {
   Typography,
   Container,
 } from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock';
 
 export default function Register() {
-  return <div></div>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3}>
+        <Avatar>
+          <LockIcon />
+        </Avatar>
+      </Paper>
+    </Container>
+  );
 }
