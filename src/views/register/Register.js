e009c3de5cba1bbfa2a7db49a9f955e324d7e2ee
winny1212 <maomaodragon1212@gmail.com>
2021-12-10 11:53:58 +1100

    add the styling of login container

--- src/views/register/Register.js
+++ src/views/register/Register.js
@@ -8,12 +8,15 @@ import {
   Container,
 } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
+import useStyles from './styles';
 
 export default function Register() {
+  const classes = useStyles();
+
   return (
     <Container component="main" maxWidth="xs">
-      <Paper elevation={3}>
-        <Avatar>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
           <LockIcon />
         </Avatar>
       </Paper>
