3dcefb222e94e64a639a5392618eb3c839414992
winny1212 <maomaodragon1212@gmail.com>
2021-12-10 11:58:49 +1100

    check the error

--- src/views/register/styles.js
+++ src/views/register/styles.js
@@ -3,10 +3,8 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles(() => ({
   paper: {
     padding: '10px 30px',
-    // marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    // padding: theme.spacing(2),
   },
 }));
