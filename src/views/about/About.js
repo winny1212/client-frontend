7f376dcf81d822210f9cdbae292526315b8a47e0
natacha <nat_bartu@hotmail.com>
2021-12-10 15:05:34 +1100

    add card and image to about page

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import AboutImage from '../../assets/img/dogs/home_grooming_02.jpg.jpg';
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="100%"
+            image={AboutImage}
+            alt="diy-grooming"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              DIY Grooming
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              DIY dog grooming instructions. It can be accessed by the Notting
+              Hill neighbourhood community as well as other people interested in
+              the DIY dog grooming. Anyone including professional groomers can
+              register and share their tips and tricks. Additionally,
+              professional dog groomers can promote their services and possibly
+              be contacted when their service is needed.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
   );
 }
 
