ddd90e7c8e1c962e13c60642d4c613a7ca0a8c6d
winny1212 <maomaodragon1212@gmail.com>
2021-12-10 14:53:19 +1100

    add icon from material ui

---
+++ src/views/register/Input.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+export default Input;
