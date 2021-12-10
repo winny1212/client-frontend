dcd4ad8ed25b38b2b677076b1bfbd6df635ab669
winny1212 <maomaodragon1212@gmail.com>
2021-12-10 14:52:00 +1100

    add the email and password form

--- src/views/register/Register.js
+++ src/views/register/Register.js
@@ -5,14 +5,18 @@ import {
   Paper,
   Grid,
   Typography,
+  TextField,
   Container,
 } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import useStyles from './styles';
+import Input from './Input';
 
 export default function Register() {
   const classes = useStyles();
   const isSignup = false;
+  const handleChange = () => {};
+  const handleSubmit = () => {};
 
   return (
     <Container component="main" maxWidth="xs">
@@ -24,6 +28,24 @@ export default function Register() {
         <Typography component="h1" variant="h5">
           {isSignup ? 'Sign up' : 'Sign in'}
         </Typography>
+
+        <form>
+          <Grid container spacing={2}>
+            {isSignup && <></>}
+
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+            />
+          </Grid>
+        </form>
       </Paper>
     </Container>
   );
