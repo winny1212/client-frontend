3e2d7005a906fefc0386fc7c147f3ae2fd02c469
a-sh <iam.angie@ymail.com>
2021-12-11 18:37:34 +1100

    Configure DIYtheme's typography

--- src/components/styles/DIYtheme.js
+++ src/components/styles/DIYtheme.js
@@ -1,5 +1,11 @@
+import '../../index.css';
 import { createTheme } from '@mui/material/styles';
 import { grey, pink } from '@mui/material/colors/';
+
+/* 400, 700 */
+/* font-family: 'Libre Baskerville', serif; */
+/* 400, 600, 800 */
+/* font-family: 'Montserrat', sans-serif; */
 
 // set global reusable variables
 const g = {
@@ -9,10 +15,14 @@ const g = {
   // accent colors
   accentYellow: 'rgba(236, 197, 144, 1)',
   accentPink: 'rgba(203, 133, 168, 1)',
+  // fonts
+  fontPrimary: `'Libre Baskerville', serif`,
+  fontSecondary: `'Montserrat', sans-serif`,
 };
 
 const theme = (mode) =>
   createTheme({
+    // color settings
     palette: {
       mode: 'light',
       primary: {
@@ -29,6 +39,56 @@ const theme = (mode) =>
       },
       text: {
         primary: g.colorPrimary,
+      },
+    },
+    typography: {
+      fontFamily: g.fontPrimary,
+      h1: {
+        fontFamily: g.fontPrimary,
+        // fontWeight: 700,
+      },
+      h2: {
+        fontFamily: g.fontPrimary,
+        // fontWeight: 700,
+        fontStyle: 'italic',
+      },
+      h3: {
+        fontFamily: g.fontPrimary,
+        // fontWeight: 700,
+        fontStyle: 'italic',
+      },
+      h4: {
+        fontFamily: g.fontPrimary,
+        // fontWeight: 700,
+        fontStyle: 'italic',
+      },
+      body1: {
+        fontFamily: g.fontSecondary,
+      },
+      body2: {
+        fontFamily: g.fontSecondary,
+      },
+      button: {
+        fontFamily: g.fontSecondary,
+        fontWeight: 600,
+        letterSpacing: 1,
+        // textTransform: 'uppercase',
+      },
+      subtitle1: {
+        fontFamily: g.fontSecondary,
+        // fontStyle: 'italic',
+        fontWeight: 600,
+        textTransform: 'uppercase',
+      },
+      subtitle2: {
+        fontFamily: g.fontSecondary,
+        textTransform: 'uppercase',
+      },
+      caption: {
+        fontFamily: g.fontSecondary,
+      },
+      overline: {
+        fontFamily: g.fontSecondary,
       },
     },
   });
