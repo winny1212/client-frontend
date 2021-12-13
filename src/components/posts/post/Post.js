ab2491f67211798335a030f1769e6c30d8bbdf01
thangzathang <thangzathang@gmail.com>
2021-12-13 14:51:15 +1100

    Post now has access to Context API

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../../Context/UserContext';
 // import { useDispatch } from 'react-redux';
 
 // {_id, breed, dogSize, author, title, description, steps, image, likes, comments, createdAt}
 
 function Post({ post }) {
   // const dispatch = useDispatch();
+
+  // For the Edit Button.
+  const [currentId, setCurrentId] = useContext(UserContext);
 
   return (
     <div>
