2b84c889249371b704518f7b541e58b73669fa6d
a-sh <iam.angie@ymail.com>
2021-12-13 18:27:17 +1100

    Create basic PostCard layout for further tweaking

--- src/components/posts/PostCard.js
+++ src/components/posts/PostCard.js
@@ -8,6 +8,9 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const PostCard = ({ post }) => {
   return (
@@ -24,11 +27,31 @@ const PostCard = ({ post }) => {
         component="img"
         alt={post.breed}
         image={defaultImg}
-        sx={{ width: 1 / 2 }}
+        sx={{
+          width: 1 / 3,
+          border: `solid 8px #fff`,
+          display: { xs: 'none', md: 'block' },
+        }}
       />
-      <CardContent>
-        <h3>{post.title}</h3>
-        <CardActions></CardActions>
+      <CardContent sx={{ textAlign: 'left' }}>
+        <Typography variant="body2">
+          {`Dog Size - Dog Breed`.toUpperCase()}
+        </Typography>
+        <Typography variant="h6">{post.title}</Typography>
+        <Typography variant="body2">Post Author</Typography>
+        <Box sx={{ alignItems: 'end' }}>
+          <CardActions>
+            <Typography variant="body2">
+              {new Date(post.createdAt).toDateString()}
+            </Typography>
+            <IconButton aria-label="like">
+              <FavoriteIcon color="accentPink" />
+            </IconButton>
+            <IconButton aria-label="comment">
+              <CommentIcon color="accentPink" />
+            </IconButton>
+          </CardActions>
+        </Box>
       </CardContent>
     </Card>
   );
