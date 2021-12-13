8ea471b7ae3d76809fff912cb1592c2d3aa19773
thangzathang <thangzathang@gmail.com>
2021-12-13 14:26:59 +1100

    Added the User context

--- src/views/form/Form.js
+++ src/views/form/Form.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 // Redux State
 import { useDispatch } from 'react-redux';
+// User Context
+import { UserContext } from '../../Context/UserContext';
 // Dispatch Action
 import { createPost } from '../../actions/posts';
 
@@ -12,6 +14,9 @@ import useStyles from './styles';
 import { Paper, Container, TextField, Typography, Button } from '@mui/material';
 
 function Form() {
+  // User Context
+  const { currentId, setCurrentId } = useContext(UserContext);
+
   // Invoke Redux State
   const dispatch = useDispatch();
 
