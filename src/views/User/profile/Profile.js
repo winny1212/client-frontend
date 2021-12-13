f257eab4e03f6f6870cfcc837377c8872ae3693e
natacha <nat_bartu@hotmail.com>
2021-12-13 15:47:45 +1100

    add submit button and handleSubmit function

--- src/views/User/profile/Profile.js
+++ src/views/User/profile/Profile.js
@@ -7,15 +7,19 @@ import {
   MenuItem,
   Input,
   Button,
+  Stack,
 } from '@mui/material';
-//import PhotoCamera from '@mui/icons-material';
+import SendIcon from '@mui/icons-material/Send';
 
 const Profile = () => {
   //username state
-  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
 
   //type of user state
-  const [value, setValue] = useState();
+  const [value, setValue] = useState('');
+
+  //upload photo state
+  const [photo, setPhoto] = useState('');
 
   //handle type of user
   const handleChange = (event) => {
@@ -24,54 +28,82 @@ const Profile = () => {
 
   //handle username mount
   useEffect(() => {
-    document.title = { username };
+    document.title = { name };
   });
 
   //handle username event target
-  const updateUsername = (event) => {
-    setUsername(event.target.value);
+  const updateUserName = (event) => {
+    setName(event.target.value);
+  };
+
+  //handle upload photo
+  const uploadPhotoHandler = (event) => {
+    setPhoto(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
   };
 
   return (
     <>
       <h1> Profile </h1>
       <Container>
-        <FormControl>
-          {/* usernmae */}
-          <TextField
-            id="outlined-basic"
-            label="Add your name"
-            variant="outlined"
-            value={username}
-            onChange={updateUsername}
-          />
-        </FormControl>
-        <FormControl>
-          {/* type of user */}
-          <Select value={value} id="demo-simple-select" onChange={handleChange}>
-            <MenuItem value={'Professional Groomer'}>
-              Professional Groomer
-            </MenuItem>
-
-            <MenuItem value={'Non Professional Groomer'}>
-              Non Professional Groomer
-            </MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl>
-          {/* upload button */}
-          <label htmlFor="contained-button-file">
-            <Input
-              accept="image/*"
-              id="contained-button-file"
-              multiple
-              type="file"
+        <form onSubmit={handleSubmit}>
+          {/* user name */}
+          <FormControl>
+            <TextField
+              id="outlined-basic"
+              label="Add your name"
+              variant="outlined"
+              value={name}
+              onChange={updateUserName}
             />
-            <Button variant="contained" component="span">
-              Upload
-            </Button>
-          </label>
-        </FormControl>
+          </FormControl>
+
+          {/* type of user */}
+          <FormControl>
+            <Select
+              value={value}
+              id="demo-simple-select"
+              onChange={handleChange}
+            >
+              <MenuItem value={'Professional Groomer'}>
+                Professional Groomer
+              </MenuItem>
+
+              <MenuItem value={'Non Professional Groomer'}>
+                Non Professional Groomer
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            {/* upload button */}
+            <label htmlFor="contained-button-file">
+              <Input
+                accept="image/*"
+                id="contained-button-file"
+                // multiple
+                type="file"
+                value={photo}
+              />
+              <Button
+                variant="contained"
+                component="span"
+                onChange={uploadPhotoHandler}
+              >
+                Upload Photo
+              </Button>
+            </label>
+          </FormControl>
+          <div>
+            <Stack direction="row" spacing={2}>
+              <Button variant="contained" endIcon={<SendIcon />}>
+                Submit
+              </Button>
+            </Stack>
+          </div>
+        </form>
       </Container>
     </>
   );
