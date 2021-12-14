be71655d6ae8803842868cde82fbf0760cecd685
natacha <nat_bartu@hotmail.com>
2021-12-15 10:28:53 +1100

    add location, email, website field into the form

--- src/views/User/profile/ProfileForm.js
+++ src/views/User/profile/ProfileForm.js
@@ -15,18 +15,27 @@ import {
 //   display: 'none',
 // });
 
-const Profile = () => {
-  //user name state
+const ProfileForm = () => {
+  //username state
   const [name, setName] = useState('');
 
-  //user location state
+  // email state
+  const [email, setEmail] = useState('');
+
+  // location state
   const [location, setLocation] = useState('');
 
-  //error user name
+  // website state
+  const [website, setWebsite] = useState('');
+
+  //name validation
   const [nameError, setNameError] = useState(false);
 
-  //error location
+  //location validation
   const [locationError, setLocationError] = useState(false);
+
+  //email validation
+  const [emailError, setEmailError] = useState(false);
 
   //set radio button to the state
   const [category, setCategory] = useState('');
@@ -42,6 +51,7 @@ const Profile = () => {
     event.preventDefault();
     setNameError(false);
     setLocationError(false);
+    setEmailError(false);
 
     console.log(file.name);
     if (name === '') {
@@ -50,9 +60,12 @@ const Profile = () => {
     if (location === '') {
       setLocationError(true);
     }
+    if (email === '') {
+      setEmailError(true);
+    }
 
-    if (name && fileSelected) {
-      console.log(name, category, fileSelected, bio);
+    if (name) {
+      console.log(name, category, fileSelected, bio, location, email, website);
     }
     // //handle upload data
     // const formData = new FormData();
@@ -84,79 +97,88 @@ const Profile = () => {
       <h1> Profile </h1>
       <form onSubmit={handleSubmit}>
         <FormControl>
-          {/* user name */}
-          <FormControl>
-            <TextField
-              id="outlined-basic"
-              label="Add your name"
-              variant="outlined"
-              fullWidth
-              required
-              onChange={(e) => setName(e.target.value)}
-              error={nameError}
-            />
-            <br />
-            {/* user location */}
-            <TextField
-              id="outlined-basic"
-              label="Add you Location"
-              variant="outlined"
-              fullWidth
-              required
-              onChange={(e) => setLocation(e.target.value)}
-              error={locationError}
-            />
-          </FormControl>
+          {/* username */}
+          <TextField
+            id="outlined-basic"
+            label="Add your name"
+            variant="outlined"
+            fullWidth
+            required
+            onChange={(e) => setName(e.target.value)}
+            error={nameError}
+          />
           <br />
-
+          {/* location */}
+          <TextField
+            id="outlined-basic"
+            label="Add your Location"
+            variant="outlined"
+            fullWidth
+            required
+            onChange={(e) => setLocation(e.target.value)}
+            error={locationError}
+          />
+          <br />
+          {/* email */}
+          <TextField
+            id="outlined-basic"
+            label="Add your Email"
+            variant="outlined"
+            fullWidth
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            error={emailError}
+          />
+          <br />
+          {/* website */}
+          <TextField
+            id="outlined-basic"
+            label="Add your website"
+            variant="outlined"
+            fullWidth
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+          <br />
           {/* type of user radio button */}
-          <FormControl component="fieldset">
-            <FormLabel component="legend">
-              Are you professional groomer?
-            </FormLabel>
-            <RadioGroup
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              aria-label="type of user"
-              defaultValue="no"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel value="no" control={<Radio />} label="no" />
-              <FormControlLabel value="yes" control={<Radio />} label="yes" />
-            </RadioGroup>
-          </FormControl>
-
+          <FormLabel component="legend">
+            Are you professional groomer?
+          </FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            aria-label="type of user"
+            defaultValue="no"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="no" control={<Radio />} label="no" />
+            <FormControlLabel value="yes" control={<Radio />} label="yes" />
+          </RadioGroup>
           <br />
-          <FormControl>
-            {/* upload button */}
-            <p>Please, add your photo!</p>
-            <label htmlFor="contained-button-file">
-              <Input
-                style={{ display: 'none' }}
-                accept="image/*"
-                id="contained-button-file"
-                multiple
-                type="file"
-                onChange={changeHandler}
-              />
-              <Button variant="contained" component="span">
-                Upload Photo
-              </Button>
-            </label>
-          </FormControl>
-          <br />
-          <div>
-            <TextField
-              id="outlined-multiline-static"
-              label="Add you Bio"
-              multiline
-              rows={4}
-              value={bio}
-              onChange={(e) => setBio(e.target.value)}
+          {/* upload button */}
+          <p>Please, add your photo!</p>
+          <label htmlFor="contained-button-file">
+            <Input
+              style={{ display: 'none' }}
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+              onChange={changeHandler}
             />
-          </div>
+            <Button variant="contained" component="span">
+              Upload Photo
+            </Button>
+          </label>
           <br />
-
+          <TextField
+            id="outlined-multiline-static"
+            label="Add you Bio"
+            multiline
+            rows={4}
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+          />
+          <br />
           <Button type="submit" variant="contained">
             Submit
           </Button>
@@ -166,4 +188,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default ProfileForm;
