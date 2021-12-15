c09fd773188bf2f77bfddd82c95e5b0ee738e783
winny1212 <maomaodragon1212@gmail.com>
2021-12-15 12:45:29 +1100

    set up the carousel structure from the data

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -4,5 +4,26 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import data from './dataSlider';
 
 export default function About() {
-  return <div></div>;
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>About
+        </h2>
+      </div>
+      <div className="section-center">
+        {image.map((image, imageIndex) => {
+          const { id, image_link, title } = image;
+
+          return (
+            <article className={position} key={id}>
+              <img src={image_link} alt={title} className="person-img" />
+
+              <p className="title">{title}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
