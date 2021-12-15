22e2fd1675f5a2860af09ea04b5e715868f332c6
winny1212 <maomaodragon1212@gmail.com>
2021-12-15 12:36:16 +1100

    import the data of carousel function

---
+++ src/views/about/dataSlider.js
@@ -0,0 +1,24 @@
+const dataSlider = [
+  {
+    id: 1,
+    title: 'Hi, I am under washing',
+    image_link: '../imgs/img1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Hi, I am under grooming',
+    image_link: '../imgs/img2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Hi, I like kepping clean and tidy',
+    image_link: '../imgs/img3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Hi, welcome to the DIY grooming.',
+    image_link: '../imgs/img4.jpg',
+  },
+];
+
+export default dataSlider;
