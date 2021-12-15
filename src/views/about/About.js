90ca99230a3cc50ad040df5b68191e304daec1d7
winny1212 <maomaodragon1212@gmail.com>
2021-12-15 13:57:35 +1100

    about blog introduction

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -19,6 +19,7 @@ export default function About() {
       setIndex(0);
     }
   }, [index, image]);
+
   // the function for image carousel, the image will slide each second
   useEffect(() => {
     let slider = setInterval(() => {
@@ -28,6 +29,7 @@ export default function About() {
       clearInterval(slider);
     };
   }, [index]);
+
   return (
     <section className="section">
       <div className="title">
@@ -55,6 +57,30 @@ export default function About() {
         <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
+      </div>
+
+      <div className="para">
+        <h3>Welcome to the Notting Hill Dog DIY Blog</h3>
+        <p>
+          Welcome to the Notting Hill Community Dog Blog! We are Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Dicta, ullam minima.
+          Maiores nisi iste sit necessitatibus officiis cumque distinctio
+          accusamus nesciunt asperiores dolore iusto inventore fugit ut
+          quisquam, modi saepe. Labore perferendis rem minus eius excepturi
+          aliquam eos, adipisci iure vel sunt odit, eaque qui laboriosam. Rem
+          inventore sunt sequi tempora dignissimos, debitis eligendi iste soluta
+          autem quasi explicabo excepturi. Consectetur laborum ex non minus
+          commodi doloremque dolores unde molestiae inventore? Ratione quidem
+          similique veritatis debitis consequatur maxime doloribus nemo itaque
+          aperiam quisquam magni, provident cumque sed beatae rem! Veritatis.
+          Voluptates sit commodi, porro ad quod in eum nobis totam soluta
+          aspernatur magni dicta laudantium debitis tenetur magnam adipisci, cum
+          fugiat perspiciatis voluptatum veritatis illo fugit dolore. Culpa,
+          dolorum magni. Sapiente deleniti voluptates officia nemo culpa dolor
+          at enim, corrupti facilis! Voluptas illum eius exercitationem
+          molestias, pariatur expedita voluptates hic error voluptatem, nesciunt
+          reiciendis dicta fugit. Ex doloribus corrupti voluptas.
+        </p>
       </div>
     </section>
   );
