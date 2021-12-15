1e035c0b6e582a219de12e09ee66571204054422
winny1212 <maomaodragon1212@gmail.com>
2021-12-15 12:42:41 +1100

    import react-icon

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,42 +1,8 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import AboutImage from '../../assets/img/dogs/home_grooming_02.jpg.jpg';
-import { useStyles } from '../../views/about/styles';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
-function About() {
-  const classes = useStyles();
+import data from './dataSlider';
 
-  return (
-    <>
-      <Card className={classes.container}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="100%"
-            image={AboutImage}
-            alt="diy-grooming"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              DIY Grooming
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              DIY dog grooming instructions. It can be accessed by the Notting
-              Hill neighbourhood community as well as other people interested in
-              the DIY dog grooming. Anyone including professional groomers can
-              register and share their tips and tricks. Additionally,
-              professional dog groomers can promote their services and possibly
-              be contacted when their service is needed.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </>
-  );
+export default function About() {
+  return <div></div>;
 }
-
-export default About;
