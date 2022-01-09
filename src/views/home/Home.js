6b3aa6d00e4402dbd641e44a116dc8ddbaea513a
winny1212 <maomaodragon1212@gmail.com>
2022-01-10 10:00:53 +1100

    import the styled-components and setup searchBar structure

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -10,6 +10,7 @@ import { UserContext } from '../../context/UserContext';
 
 // MUI
 import Container from '@mui/material/Container';
+import SearchBar from '../../components/searchbar/SearchBar';
 // import Grid from '@mui/material/Grid';
 // import Box from '@mui/material/Box';
 // import Typography from '@mui/material/Typography';
@@ -29,6 +30,8 @@ function Home() {
       <Header title="Find tips & tricks to groom your beloved pets" />
       <Container maxWidth="lg">
         <h3>Searchbar</h3>
+        {/* import search and filter function components */}
+        <SearchBar />
         <h3>Featured Posts</h3>
         <Posts />
       </Container>
