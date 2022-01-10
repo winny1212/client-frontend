b08c1ac74278c2c91f4a2b12d33a0348fd0c50f8
a-sh <iam.angie@ymail.com>
2022-01-10 14:14:21 +1100

    (WIP) Modify StepsForm, add Lists component

--- src/components/posts/forms/StepsForm.js
+++ src/components/posts/forms/StepsForm.js
@@ -2,19 +2,21 @@ import React, { useState } from 'react';
 
 // MUI
 import Box from '@mui/material/Box';
-import Box from '@mui/material/Box';
 // import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 // Components
 import FormInput from '../../shared/FormInput';
 import Hr from '../../shared/Hr';
 import { StyledBtnOutlined } from '../../shared/StyledButtons';
 
-function Steps() {
+function Steps({ handleChange, postData, setPostData }) {
   // state for lists of steps
-  const [stepsList, setStepsList] = useState([]);
+  const [stepsList, setStepsList] = useState(postData.steps);
   // state for each step
   const [step, setStep] = useState('');
 
@@ -41,7 +43,7 @@ function Steps() {
         Grooming instructions preview
       </Typography>
 
-      <Grid></Grid>
+      <List></List>
     </>
   );
 }
