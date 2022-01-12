7fdcb012dfc4711793c39cdb07bdcae78c7c05b9
thangzathang <59811225+thangzathang@users.noreply.github.com>
2022-01-12 17:02:31 +1100

    imported useStyles

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -15,6 +15,7 @@ import { UserContext } from '../../context/UserContext';
 // MUI
 import Container from '@mui/material/Container';
 import FilterBar from '../../components/filterBar/FilterBar';
+import useStyles from './styles';
 
 // import Grid from '@mui/material/Grid';
 // import Box from '@mui/material/Box';
