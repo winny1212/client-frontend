ee45d23ad912be3ccaea0b9a86aabdc1eb896baa
a-sh <iam.angie@ymail.com>
2022-01-13 17:26:33 +1100

    Configure firebase dotenv & place firebase hook in UserContext

--- src/components/posts/forms/PostForm.js
+++ src/components/posts/forms/PostForm.js
@@ -7,18 +7,18 @@ import { useDispatch } from 'react-redux';
 import { createPost } from '../../../actions/posts';
 
 // Components & Data
-import PostContextProvider from '../../../context/PostContext';
 import StepsForm from './StepsForm';
 import BaseLayout from '../../shared/BaseLayout';
+import { StyledBtn, StyledBtnOutlined } from '../../shared/StyledButtons';
+import DetailsForm from './DetailsForm';
 
 // Context
 import { usePostContext } from '../../../context/PostContext';
+import { useUserContext } from '../../../context/UserContext';
 
 // MUI
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
-import { StyledBtn, StyledBtnOutlined } from '../../shared/StyledButtons';
-import DetailsForm from './DetailsForm';
 
 // {_id, breed, dogSize, username, title, duration, description, steps, image, likes, comments, createdAt}
 
@@ -30,48 +30,9 @@ function PostForm() {
   const currentUser = JSON.parse(localStorage.getItem('profile'));
 
   // PostContext consume
-  const {
-    postData,
-    setPostData,
-    initialDetailsData,
-    details,
-    setDetails,
-    stepsTempLocal,
-    selectedBreed,
-    setSelectedBreed,
-    instructions,
-    handlePostPublish,
-  } = usePostContext();
-
-  // Clear all inputs to initial state
-  const clearInputs = () => setDetails(initialDetailsData);
-
-  // // Create POST to send to backend
-  // const handlePostPublish = (e) => {
-  //   e.preventDefault();
-
-  //   const newPost = setPostData({
-  //     ...details,
-  //     steps: instructions,
-  //     breed: selectedBreed.label,
-  //     username: currentUser?.result?.username,
-  //   });
-  //   // add alert to notify post is published?
-
-  //   // Send post to backend - CREATE POST
-  //   dispatch(createPost(newPost));
-
-  //   // clear inputs back to initial values
-  //   clearInputs();
-  //   // clear temporary local storage for steps
-  //   localStorage.removeItem(stepsTempLocal);
-  //   // set breed back to null
-  //   setSelectedBreed(null);
-
-  //   console.log('Post Published!');
-  //   console.log('-- postData:\n', JSON.stringify(postData, null, 2));
-  //   // redirect page to the new post
-  // };
+  const { postData, handlePostPublish } = usePostContext();
+  // UserContext consume
+  const { firebase } = useUserContext();
 
   return (
     <>
@@ -94,8 +55,6 @@ function PostForm() {
             </Grid>
           </Grid>
         </form>
-        <h3>POST DATA PREVIEW</h3>
-        <p>{JSON.stringify(postData, null, 2)}</p>
       </BaseLayout>
     </>
   );
