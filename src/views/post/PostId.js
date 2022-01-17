fb7ceb4f905a6f17f1c827818d55f7120c2a4ea5
thangzathang <thangzathang@gmail.com>
2022-01-17 11:49:14 +1100

    Removed bug

--- src/views/post/PostId.js
+++ src/views/post/PostId.js
@@ -10,18 +10,13 @@ import CommentSection from './CommentSection';
 // MUI
 import Container from '@mui/material/Container';
 
-import { useDispatch } from 'react-redux';
-import { getAuthor } from '../../actions/auth';
-
 const PostId = () => {
-  const dispatch = useDispatch();
   // define the post/:id params from App.js
   const { postID } = useParams();
   // const location = useLocation();
   // const postPath = location.pathname.split('/')[2];
 
   const [post, setPost] = useState({});
-  const [author, setAuthor] = useState({});
 
   useEffect(() => {
     const getPost = async () => {
@@ -30,10 +25,7 @@ const PostId = () => {
       setPost(res.data);
     };
     getPost();
-
-    const user = dispatch(getAuthor(post.authorId));
-    setAuthor(user);
-  }, [postID, post.authorId, dispatch]);
+  }, [postID]);
 
   console.log('POST is:', post);
   console.log('POST user ID is:', post.authorId);
@@ -46,7 +38,7 @@ const PostId = () => {
     <>
       <Header title={post.title} />
       <Container maxWidth="lg">
-        <Post post={post} author={author} />
+        <Post post={post} />
         {/* <h3>Comment Form</h3>
         <h3>Comments</h3> */}
         <CommentSection post={post} postID={postID} />
